5e9dd4ba52c56bad37a7be47598c399bc6bcceb0
SKS1401 <srabosti.k@gmail.com>
2024-11-12 15:21:17 +0000

    some data conversion

---
+++ 03_dataconversion.js
@@ -0,0 +1,15 @@
+let str1= "Hello"
+let str2=" Shoibo"
+let str3= str1+str2;
+// console.log(str3);
+// console.log("1"+2+2)
+// console.log(2+2+"0")
+let x=1;
+let y=++x;//prefix
+console.log(x,y)
+let a=1;
+let b=a++;//postfix
+console.log(a,b);
+
+
+
