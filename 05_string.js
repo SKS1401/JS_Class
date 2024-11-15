08732d1a9a619fc5fcd7cd7c24209b634f0d3f1a
SKS1401 <srabosti.k@gmail.com>
2024-11-15 09:53:24 +0000

    class on string

---
+++ 05_string.js
@@ -0,0 +1,20 @@
+const gamename="SKS1401";
+const n=50;
+console.log(`Hi! My GameID is ${gamename} and My repo count is ${n}`);
+const newString=new String("Srabosti");
+// console.log(gamename.length);
+// console.log(gamename.charAt(4));
+// console.log(gamename.lastIndexOf("1"));
+// console.log(newString.slice(3,6));
+// console.log(newString.slice(-6,-3));//slice accepts - index
+console.log(newString.substring(0,4));
+//substring() and substr() and slice() study from following link
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/substring
+
+
+
+
+
+
+
+
